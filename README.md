7f5caa8f8847fd819b49ffd4edcc27d4941164f2
Diya Kharb <diyakharb1029@gmail.com>
2026-01-04 00:43:42 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,10 @@
+# Extra-Project--Quiz-Website
+
+In this project, users can seamlessly access the login/signup function directly from the home page, swiftly immersing themselves into the captivating quiz experience. The quiz page have an aesthetic layout complemented by interactive functionalities, enhancing user engagement and enjoyment. 
+
+<img width="1426" alt="Screenshot 2024-04-25 at 3 52 14 PM" src="https://github.com/abhik1029/Extra-Project--Quiz-Website/assets/164001169/5b28be5d-0982-4dfa-b4fe-349ffac2087d">
+<img width="1417" alt="Screenshot 2024-04-25 at 3 52 25 PM" src="https://github.com/abhik1029/Extra-Project--Quiz-Website/assets/164001169/a65a39d7-79f7-4c2f-ba2d-5a8d7842cad7">
+<img width="1421" alt="Screenshot 2024-04-25 at 3 52 40 PM" src="https://github.com/abhik1029/Extra-Project--Quiz-Website/assets/164001169/a536949f-4627-47f0-90d3-e8e8f76045b2">
+<img width="1430" alt="Screenshot 2024-04-25 at 3 52 51 PM" src="https://github.com/abhik1029/Extra-Project--Quiz-Website/assets/164001169/729a8a8a-1a73-44d8-adf2-469fa4f6d61e">
+<img width="1426" alt="Screenshot 2024-04-25 at 3 53 03 PM" src="https://github.com/abhik1029/Extra-Project--Quiz-Website/assets/164001169/97020245-b14b-4a13-968a-1232ef7b72ea">
+<img width="1055" alt="Screenshot 2024-04-25 at 3 53 22 PM" src="https://github.com/abhik1029/Extra-Project--Quiz-Website/assets/164001169/d89929d1-c36d-4f22-8e0f-00487a4713bf">
---
+++ js/questions.js
@@ -0,0 +1,138 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+    numb: 1,
+    question:
+      "Which of the following CSS Property sets the pace of the transition to the next keyframe?",
+    answer: "Animation-transition-function",
+    options: [
+      "Animation-pace-function",
+      "Animation-transition-function",
+      "Animation-timing-function",
+      "None of the above",
+    ],
+  },
+  {
+    numb: 2,
+    question: "Which of the following is a valid HTTP Method?",
+    answer: "Get",
+    options: ["Get", "Put", "Post", "All of the above"],
+  },
+  {
+    numb: 3,
+    question: "On which of the Architectural pattern AngularJS is based?",
+    answer: "MVC Architecture Pattern",
+    options: [
+      "Observer Pattern",
+      "Decorator Pattern",
+      "MVC Architecture Pattern",
+      "MVVM Architectural pattern",
+    ],
+  },
+  {
+    numb: 4,
+    question: "What function is used to fire an event?",
+    answer: "None of the above",
+    options: ["Emit()", "fire()", "Calc()", "None of the above"],
+  },
+  {
+    numb: 5,
+    question:
+      "Which of the following pattern is taken by the buffer.toString Method?",
+    answer: "Encoding",
+    options: ["Encoding", "Start", "End", "All of the above"],
+  },
+  {
+    numb: 6,
+    question: "All APIs of NodeJS are?",
+    answer: "None of the above",
+    options: [
+      "Asynchronous",
+      "Synchronous",
+      "None of the above",
+      "All of the above",
+    ],
+  },
+  {
+    numb: 7,
+    question: "What is Express.js?",
+    answer: "A Web server framework for Node.js",
+    options: [
+      "A database",
+      "A frontend framework",
+      "A Web server framework for Node.js",
+      "An operating system",
+    ],
+  },
+  {
+    numb: 8,
+    question: " The functionality of angular controllers in the application is",
+    answer: "None of the above",
+    options: [
+      "Used for displaying the data",
+      "Used for controlling the data",
+      "None of the above",
+      "All of the above",
+    ],
+  },
+  {
+    numb: 9,
+    question: "Which node module is used to handle binary data?",
+    answer: "Buffer",
+    options: ["Binary", "Buffer", "Crypto", "Dgram"],
+  },
+  {
+    numb: 10,
+    question:
+      "Which of the following is a popular front-end framework for building user interfaces in JavaScript?",
+    answer: "Angular",
+    options: ["Django", "Angular", "Flask", "Node.js"],
+  },
+  //
+  {
+    numb: 11,
+    question: "What is callback?",
+    answer: "None of the above",
+    options: [
+      "The callback is a technique in which a method calls back the caller method",
+      "The callback is a asynchronous equivalent for a function",
+      "None of the above",
+      "All of the above",
+    ],
+  },
+  {
+    numb: 12,
+    question: " The total $RootScope an AngularJS application can have is?",
+    answer: "None of the above",
+    options: ["0", "1", "2", "None of the above"],
+  },
+  {
+    numb: 13,
+    question:
+      " Which of the following is a core component in an Angular application?",
+    answer: "Directive",
+    options: ["ViewController", "Directive", "Activity", "Observer"],
+  },
+  {
+    numb: 14,
+    question: "AngularJS is a ",
+    answer: "Javascript framework",
+    options: [
+      "Java framework",
+      "Javascript framework",
+      "HTML Framework",
+      "Sql framework",
+    ],
+  },
+  {
+    numb: 15,
+    question: "Which of the following are the benefits of using modules?",
+    answer: "All of the above",
+    options: [
+      "Provides a mean of dividing up tasks ",
+      "Provides a mean of reusing program codes",
+      "Provides a mean of testing individual parts of a program",
+      "All of the above",
+    ],
+  },
+];
